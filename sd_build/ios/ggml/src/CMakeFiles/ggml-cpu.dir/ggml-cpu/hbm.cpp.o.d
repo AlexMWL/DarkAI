c0ee833b2ba98556e238ee2884bad6641fd46ec0
Lex W. <38472888+AlexMWL@users.noreply.github.com>
2026-07-08 19:13:54 -0700

    Delete sd_build directory

--- sd_build/ios/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o.d
+++
@@ -1,3 +0,0 @@
-ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o: \
-  /Applications/Xcode-beta.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS27.0.sdk/SDKSettings.json \
-  /Users/alexmwl/Desktop/DarkAI/stable-diffusion.cpp/ggml/src/ggml-cpu/hbm.cpp
--- sd_build/ios/thirdparty/libwebm/CMakeFiles/webvtt_common.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for webvtt_common.
-# This may be replaced when dependencies are built.
